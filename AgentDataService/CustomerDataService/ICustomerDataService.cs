56724861b4e4eed5fddf916f405084a3fe838631
Max Wiggs <maxwiggs02@gmail.com>
2019-02-18 03:08:25 +0000

    built for release

--- AgentDataService/CustomerDataService/ICustomerDataService.cs
+++ AgentDataService/CustomerDataService/ICustomerDataService.cs
@@ -4,5 +4,8 @@
     {
         string AllCustomers();
         string CustomerByAgent(int id);
+        string AddCustomer(string data);
+        string DeleteCustomer(int id);
+        string UpdateCustomer(string data);
     }
 }
