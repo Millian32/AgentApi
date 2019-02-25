8f17bff6a5c539a06605ee14129efc79a1cad124
Max Wiggs <maxwiggs02@gmail.com>
2019-02-25 22:08:09 +0000

    added application insights

--- AgentApi/Program.cs
+++ AgentApi/Program.cs
@@ -12,6 +12,7 @@ namespace AgentApi
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseApplicationInsights()
                 .UseStartup<Startup>();
     }
 }
